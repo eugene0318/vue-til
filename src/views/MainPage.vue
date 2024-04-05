a02153ee51413313e379a19e5681526ff7d28fe9
eugene <nttott@gmail.com>
2024-04-05 16:44:46 +0900

    loadingSpinner

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,14 +2,57 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+
+      <ul>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        >
+        </PostListItem>
+        <!-- <li v-for="postItem in postItems" :key="postItem._id">
+          <div class="post-title">
+            {{ postItem.title }}
+          </div>
+          <div class="post-contents">
+            {{ postItem.contents }}
+          </div>
+          <div class="post-time">
+            {{ postItem.createdAt }}
+          </div>
+        </li> -->
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from "@/components/posts/PostListItem.vue";
+import LoadingSpinner from "@/components/common/LoadingSpinner.vue";
+import { fetchPosts } from "../api";
+
 export default {
+  comments: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
   methods: {
-    fetchNotes() {},
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      this.isLoading = false;
+      this.postItems = data.postItems;
+    },
+  },
+  created() {
+    this.fetchData();
   },
 };
 </script>
